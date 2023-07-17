3297775f0660a14360725137fe89c32c122d1f89
Vasantharaja <98202074+vasantharaja200295@users.noreply.github.com>
2023-07-17 09:27:49 +0530

    changed navebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <NavBar/>
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+        <nav>
+            <li>home</li>
+        </nav>
+    </div>
   )
 }
 
